9a8b81085556b5bf8975a12042a90dee989b841f
Nicola Tobias Seitz <seitznicolatobias@gmail.com>
2024-11-30 19:03:38 +0100

    2023/04 part 2

--- 2023/04/Puzzel4.py
+++ 2023/04/Puzzel4.py
@@ -3,19 +3,26 @@ import re
 import unittest
 
 
-def solve(input_file_path: str) -> tuple:
+def solve(input_file_path: str) -> int:
     with open(input_file_path, "r") as file:
+        lines = file.readlines()
         scratch_card_worth = 0
-        for line in file:
+        copies = [1] * len(lines)
+        for idx, line in enumerate(lines):
             (winning_numbers, your_numbers) = line.split(":")[1].split("|")
             set_winning_numbers = set(winning_numbers.split())
             set_your_numbers = set(your_numbers.split())
 
             your_win_numbers = set_your_numbers & set_winning_numbers
-            if len(your_win_numbers) > 0:
-                scratch_card_worth += 2 ** (len(your_win_numbers) - 1)
+            count_of_your_win_numbers = len(your_win_numbers)
 
-        return scratch_card_worth, 0
+            if count_of_your_win_numbers > 0:
+                scratch_card_worth += 2 ** (count_of_your_win_numbers - 1)
+
+            for i in range(1, 1 + count_of_your_win_numbers):
+                copies[idx + i] += copies[idx]
+
+        return scratch_card_worth, sum(copies)
 
 
 class Test(unittest.TestCase):
@@ -24,7 +31,7 @@ class Test(unittest.TestCase):
             os.path.dirname(os.path.abspath(__file__)), "testData.txt"
         )
 
-        self.assertEqual(solve(input_file_path), (13, 0))
+        self.assertEqual(solve(input_file_path), (13, 30))
 
 
 if __name__ == "__main__":
