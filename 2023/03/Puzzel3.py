81b1ead0faff7a7a4acc3ec0fdce71e952767fe5
Nicola Tobias Seitz <seitznicolatobias@gmail.com>
2024-11-30 18:11:40 +0100

    2023/03 part 2

--- 2023/03/Puzzel3.py
+++ 2023/03/Puzzel3.py
@@ -2,38 +2,83 @@ import os
 import re
 import unittest
 
+from functools import reduce
 
-def solve(input_file_path: str) -> int:
+adj_matrix = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+]
+
+
+def solve(input_file_path: str) -> tuple:
     with open(input_file_path, "r") as file:
-        sum = 0
-        char_array = [list(line.rstrip()) for line in file]
+        numbers_coords = []
+        symbols_coords = []
 
-        for r in range(len(char_array)):
-            current_num = ""
-            for c in range(len(char_array[r])):
-                is_digit = char_array[r][c].isdigit()
-                if c + 1 < len(char_array[r]):
-                    next_is_digit = char_array[r][c + 1].isdigit()
-                else:
-                    next_is_digit = False
+        char_matrix = [list(line.rstrip()) for line in file]
 
-                if is_digit:
-                    current_num += char_array[r][c]
-                if is_digit and not next_is_digit:
-                    for rc in range(max(0, r - 1), min(r + 2, len(char_array))):
-                        for cc in range(
-                            max(0, c - len(current_num)), min(c + 2, len(char_array[r]))
-                        ):
-                            if is_symbol(char_array[rc][cc]):
-                                sum += int(current_num)
-                                break
-                        else:
-                            continue
-                        break
+        num_rows = len(char_matrix)
+        num_cols = len(char_matrix[0])
 
-                    current_num = ""
+        for r in range(num_rows):
+            number = []
+            for c in range(num_cols):
+                if char_matrix[r][c].isdigit():
+                    number.append((r, c))
+                elif len(number) > 0:
+                    numbers_coords.append(
+                        (number, int("".join(char_matrix[x][y] for x, y in number)))
+                    )
+                    number = []
 
-        return sum
+                if is_symbol(char_matrix[r][c]):
+                    symbols_coords.append((r, c))
+            else:
+                if len(number) > 0:
+                    numbers_coords.append(
+                        (number, int("".join(char_matrix[x][y] for x, y in number)))
+                    )
+                    number = []
+
+        symbol_numbers = []
+        for coords, num in numbers_coords:
+            for coord in coords:
+                is_adj = is_symbol_adjacent(coord, symbols_coords)
+                if is_adj:
+                    symbol_numbers.append(num)
+                    break
+
+        gear_numbers = []
+        gear_coords = [(x, y) for x, y in symbols_coords if char_matrix[x][y] == "*"]
+        for coords in gear_coords:
+            adj_numbers = get_adj_numbers(coords, numbers_coords)
+            if len(adj_numbers) == 2:
+                gear_numbers.append(reduce(lambda x, y: x * y, adj_numbers))
+
+        return sum(symbol_numbers), sum(gear_numbers)
+
+
+def is_symbol_adjacent(coord: tuple, symbols_coords: list[tuple]) -> bool:
+    for dx, dy in adj_matrix:
+        if (coord[0] + dx, coord[1] + dy) in symbols_coords:
+            return True
+
+    return False
+
+
+def get_adj_numbers(coord: tuple, numbers: list[tuple]) -> list[int]:
+    adj_numbers = []
+    for coords, num in numbers:
+        if any((coord[0] + dx, coord[1] + dy) in coords for dx, dy in adj_matrix):
+            adj_numbers.append(num)
+
+    return adj_numbers
 
 
 def is_symbol(char: str):
@@ -49,7 +94,7 @@ class Test(unittest.TestCase):
             os.path.dirname(os.path.abspath(__file__)), "testData.txt"
         )
 
-        self.assertEqual(solve(input_file_path), 4361)
+        self.assertEqual(solve(input_file_path), (4361, 467835))
 
 
 if __name__ == "__main__":
@@ -58,4 +103,4 @@ if __name__ == "__main__":
     input_file_path = os.path.join(
         os.path.dirname(os.path.abspath(__file__)), "data.txt"
     )
-    print(f"Result Part1: {solve(input_file_path)}")
+    print(f"Result: {solve(input_file_path)}")
