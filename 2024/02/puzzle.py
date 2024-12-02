a5d8dc1784c295c9fa1b4625eee495dbbfa0e85d
Nicola Tobias Seitz <seitznicolatobias@gmail.com>
2024-12-02 18:58:51 +0100

    2024/02 part 2

--- 2024/02/puzzle.py
+++ 2024/02/puzzle.py
@@ -5,38 +5,51 @@ MIN_DIFF = 1
 MAX_DIFF = 3
 
 
-def solve(input_file_path: str) -> int:
+def solve(input_file_path: str) -> tuple[int]:
     safe_reports = 0
+    safe_reports_damped = 0
     with open(input_file_path, "r") as file:
         for line in file:
-            numbers = line.rsplit()
-            numbers = list(map(int, numbers))
+            report_numbers = line.rsplit()
+            report_numbers = list(map(int, report_numbers))
 
-            safe_reports += 1 if is_report_safe(numbers) else 0
-    return safe_reports
+            safe_reports += 1 if is_report_safe(report_numbers) else 0
+            safe_reports_damped += 1 if is_damped_report_safe(report_numbers) else 0
+
+    return safe_reports, safe_reports_damped
 
 
-def is_report_safe(numbers):
-    numbers_length = len(numbers)
+def is_report_safe(report_numbers: list[int]) -> bool:
+    numbers_length = len(report_numbers)
 
     if numbers_length < 2:
         return False
 
-    is_increasing = numbers[0] < numbers[1]
+    is_increasing = report_numbers[0] < report_numbers[1]
 
     for idx in range(numbers_length):
         if idx == numbers_length - 1:
             return True
-        if numbers[idx] < numbers[idx + 1] and not is_increasing:
+        if report_numbers[idx] < report_numbers[idx + 1] and not is_increasing:
             return False
-        if numbers[idx] > numbers[idx + 1] and is_increasing:
+        if report_numbers[idx] > report_numbers[idx + 1] and is_increasing:
             return False
 
-        abs_diff = abs(numbers[idx] - numbers[idx + 1])
+        abs_diff = abs(report_numbers[idx] - report_numbers[idx + 1])
         if abs_diff > MAX_DIFF or abs_diff < MIN_DIFF:
             return False
 
     return True
+
+
+def is_damped_report_safe(report_numbers: list[int]) -> bool:
+    if is_report_safe(report_numbers):
+        return True
+
+    for idx in range(len(report_numbers)):
+        damped_report_numbers = report_numbers[:idx] + report_numbers[idx + 1 :]
+        if is_report_safe(damped_report_numbers):
+            return True
 
 
 class Test(unittest.TestCase):
@@ -45,7 +58,7 @@ class Test(unittest.TestCase):
             os.path.dirname(os.path.abspath(__file__)), "testData.txt"
         )
 
-        self.assertEqual(solve(input_file_path), 2)
+        self.assertEqual(solve(input_file_path), (2, 4))
 
 
 if __name__ == "__main__":
