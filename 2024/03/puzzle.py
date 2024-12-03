a8995fa3e172719cdb52e06a937997cd56946a4c
Nicola Tobias Seitz <seitznicolatobias@gmail.com>
2024-12-03 18:31:46 +0100

    2024/03 part 2

--- 2024/03/puzzle.py
+++ 2024/03/puzzle.py
@@ -5,17 +5,30 @@ from functools import reduce
 from operator import mul
 
 
-def solve(input_file_path: str) -> int:
+def solve(input_file_path: str) -> tuple:
+    sum_without_instructions = 0
+    sum_with_instructions = 0
+
     with open(input_file_path, "r") as file:
         text = file.read()
-        all_multiplications = re.findall(r"mul\(\d+,\d+\)", text)
+        all_multiplications = re.findall(r"(mul\(\d+,\d+\)|do\(\)|don't\(\))", text)
 
-    sum = 0
+    do_calculate = True
     for multiplication in all_multiplications:
-        factors = re.findall(r"\d+", multiplication)
-        sum += reduce(mul, map(int, factors))
+        if multiplication == "don't()":
+            do_calculate = False
+        elif multiplication == "do()":
+            do_calculate = True
+        else:
+            factors = re.findall(r"\d+", multiplication)
+            val = reduce(mul, map(int, factors))
 
-    return sum
+            sum_without_instructions += val
+
+            if do_calculate:
+                sum_with_instructions += val
+
+    return sum_without_instructions, sum_with_instructions
 
 
 class Test(unittest.TestCase):
@@ -24,7 +37,13 @@ class Test(unittest.TestCase):
             os.path.dirname(os.path.abspath(__file__)), "testData.txt"
         )
 
-        self.assertEqual(solve(input_file_path), 161)
+        self.assertEqual(solve(input_file_path)[0], 161)
+
+        input_file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "testData_Part2.txt"
+        )
+
+        self.assertEqual(solve(input_file_path)[1], 48)
 
 
 if __name__ == "__main__":
