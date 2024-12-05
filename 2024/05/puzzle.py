1f6331cb2e1a24e7d9d4a15ac9ab3f83e59446ad
Nicola Tobias Seitz <seitznicolatobias@gmail.com>
2024-12-05 17:53:26 +0100

    2024/05 part 1

---
+++ 2024/05/puzzle.py
@@ -0,0 +1,60 @@
+import math
+import os
+import unittest
+
+
+def solve(input_file_path: str) -> int:
+    rules = {}
+    updates = []
+    read_rules = True
+    with open(input_file_path, "r") as file:
+        for line in file:
+            line = line.rstrip()
+            if line == "":
+                read_rules = False
+                continue
+
+            if read_rules:
+                rule = line.split("|")
+                if rule[0] in rules:
+                    current_rule_data = list(rules.get(rule[0]))
+                    current_rule_data.append(rule[1])
+                    rules.update({rule[0]: current_rule_data})
+                else:
+                    rules[rule[0]] = [rule[1]]
+            else:
+                updates.append(line.split(","))
+
+    sum = 0
+    for update in updates:
+        for idx, num in enumerate(update):
+            numbers_not_allowed_before = rules.get(num, [])
+            for nnab in numbers_not_allowed_before:
+                if nnab in update[:idx]:
+                    break
+            else:
+                continue
+            break
+        else:
+            m_idx = math.ceil(len(update) / 2)
+            sum += int(update[m_idx - 1])
+
+    return sum
+
+
+class Test(unittest.TestCase):
+    def test_solve(self):
+        input_file_path = os.path.join(
+            os.path.dirname(os.path.abspath(__file__)), "testData.txt"
+        )
+
+        self.assertEqual(solve(input_file_path), 143)
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False)
+
+    input_file_path = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "data.txt"
+    )
+    print(f"Result: {solve(input_file_path)}")
