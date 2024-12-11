85420aa0d4e795e432058576443731c43416bd63
Nicola Tobias Seitz <seitznicolatobias@gmail.com>
2024-12-11 23:01:39 +0100

    2024/11 part 2

--- 2024/11/puzzle.py
+++ 2024/11/puzzle.py
@@ -8,36 +8,43 @@ def count_digits(num: int) -> int:
     return math.floor(math.log10(num) + 1)
 
 
-def update_stones(stones: list) -> list:
-    next_stones = []
+def update_stones(stones: dict) -> dict:
+    next_stones = dict()
+
     for stone in stones:
         if stone == 0:
-            next_stones.append(1)
+            z_stone = 1
+            next_stones[z_stone] = next_stones.get(z_stone, 0) + stones.get(stone, 0)
             continue
 
         digits = count_digits(stone)
         if digits % 2 == 0:
+            l_stone = int(stone // 10 ** (digits / 2))
+            r_stone = int(stone % 10 ** (digits / 2))
 
-            next_stones.append(int(stone // 10 ** (digits / 2)))
-            next_stones.append(int(stone % 10 ** (digits / 2)))
+            next_stones[l_stone] = next_stones.get(l_stone, 0) + stones.get(stone, 0)
+            next_stones[r_stone] = next_stones.get(r_stone, 0) + stones.get(stone, 0)
             continue
 
-        next_stones.append(stone * 2024)
+        m_stone = stone * 2024
+        next_stones[m_stone] = next_stones.get(m_stone, 0) + stones.get(stone, 0)
 
     return next_stones
 
 
 def solve(input_file_path: str, iterations: int) -> int:
-    stones = []
+    stones = dict()
     with open(input_file_path, "r") as file:
-        stones = list(map(int, file.readline().split()))
+        for num in file.readline().split():
+            stone = int(num)
+            stones[stone] = stones.get(stone, 0) + 1
 
     # print(f"Iteration: 0; Stones: {stones}")
     for i in range(iterations):
         stones = update_stones(stones)
-        print(f"Iteration: {i + 1}; Stones: ")
+        # print(f"Iteration: {i + 1}; Stones: {stones} ")
 
-    return len(stones)
+    return sum(stones.values())
 
 
 class Test(unittest.TestCase):
@@ -57,5 +64,8 @@ if __name__ == "__main__":
         os.path.dirname(os.path.abspath(__file__)), "data.txt"
     )
     start_time = time.time()
-    print(f"Result: {solve(input_file_path, 25)}")
+    print(f"Result Part1: {solve(input_file_path, 25)}")
+    print(f"Duration: {round(time.time() - start_time, 4)} s")
+    start_time = time.time()
+    print(f"Result Part2: {solve(input_file_path, 75)}")
     print(f"Duration: {round(time.time() - start_time, 4)} s")
