510a5b5216eb0ae8ea4f2664b974c1ccb5c0127f
Nicola Tobias Seitz <seitznicolatobias@gmail.com>
2024-12-20 15:21:58 +0100

    2024/20 time optimized

--- 2024/20/puzzle.py
+++ 2024/20/puzzle.py
@@ -1,5 +1,6 @@
 import os
 import time
+import math
 
 WALL = "#"
 EMPTY = "."
@@ -36,19 +37,22 @@ def get_parse_input(input_file_path: str) -> tuple[list, list]:
     return data_map
 
 
-def walk(data_map: list) -> list:
-    start_pos = find_position(data_map, START)
-    walked_points = [start_pos]
+def walk(data_map: list) -> dict:
+    end_pos = find_position(data_map, END)
+    last_point = end_pos
+    walked_points = {end_pos: 0}
 
-    while data_map[walked_points[-1][0]][walked_points[-1][1]] != END:
+    while data_map[last_point[0]][last_point[1]] != START:
         for x, y in ADJ_MATRIX:
-            next_pos = (x + walked_points[-1][0], y + walked_points[-1][1])
+            next_pos = (x + last_point[0], y + last_point[1])
 
             if (
                 data_map[next_pos[0]][next_pos[1]] == EMPTY
-                or data_map[next_pos[0]][next_pos[1]] == END
+                or data_map[next_pos[0]][next_pos[1]] == START
             ) and next_pos not in walked_points:
-                walked_points.append(next_pos)
+                costs = len(walked_points)
+                walked_points[next_pos] = costs
+                last_point = next_pos
                 break
 
     return walked_points
@@ -63,33 +67,44 @@ def find_position(data_map: list, find: chr) -> tuple:
     ][0]
 
 
-def find_cheats(walk_data: list) -> dict:
-    cheats = dict()
+def get_reachable_points(n):
+    reachable_points = []
 
-    for idx, pos in enumerate(walk_data):
-        for dx, dy in ADJ_MATRIX:
-            if (pos[0] + dx, pos[1] + dy) not in walk_data and (
-                pos[0] + 2 * dx,
-                pos[1] + 2 * dy,
-            ) in walk_data:
-                shortcut_idx = walk_data.index((pos[0] + 2 * dx, pos[1] + 2 * dy))
-                if shortcut_idx > idx:
-                    saved_time = shortcut_idx - idx - 2
-                    cheats[saved_time] = cheats.get(saved_time, 0) + 1
+    for row in range(-n, n + 1):
+        for col in range(-n + abs(row), n - abs(row) + 1):
+            if row != 0 or col != 0:
+                num_cheats = abs(row) + abs(col)
+                reachable_points.append([row, col, num_cheats])
+
+    return reachable_points
+
+
+def find_cheats(walk_data: dict, num_cheats: int) -> dict:
+    cheats = dict()
+    reachable_points = get_reachable_points(num_cheats)
+
+    for pos, cost in walk_data.items():
+        points_on_walk = [
+            [(pos[0] + row, pos[1] + col), nc]
+            for row, col, nc in reachable_points
+            if (pos[0] + row, pos[1] + col) in walk_data
+        ]
+
+        for p, nc in points_on_walk:
+            saved_time = cost - (walk_data.get(p) + nc)
+
+            if saved_time > 0:
+                cheats[saved_time] = cheats.get(saved_time, 0) + 1
 
     return cheats
 
 
-def solve(input_file_path: str) -> int:
+def solve(input_file_path: str, num_cheats: int) -> int:
     data_map = get_parse_input(input_file_path)
     walk_data = walk(data_map)
 
-    cheats = find_cheats(walk_data)
-
-    # print_map(data_map)
-    # print(len(walk_data) - 1)
-    # print(cheats)
-
+    cheats = find_cheats(walk_data, num_cheats)
+    # print(dict(sorted(cheats.items())))
     return sum([val for key, val in cheats.items() if key >= 100])
 
 
@@ -98,5 +113,8 @@ if __name__ == "__main__":
         os.path.dirname(os.path.abspath(__file__)), "data.txt"
     )
     start_time = time.time()
-    print(f"Result: {solve(input_file_path)}")
+    print(f"Result Part1: {solve(input_file_path, 2)}")
+    print(f"Duration: {round(time.time() - start_time, 4)} s")
+    start_time = time.time()
+    print(f"Result Part2: {solve(input_file_path, 20)}")
     print(f"Duration: {round(time.time() - start_time, 4)} s")
