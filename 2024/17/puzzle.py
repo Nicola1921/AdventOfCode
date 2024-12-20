76cc3fd08d25a670b9121ac610cf05faac4e999a
Nicola Tobias Seitz <seitznicolatobias@gmail.com>
2024-12-21 00:54:59 +0100

    2024/17 part 2

--- 2024/17/puzzle.py
+++ 2024/17/puzzle.py
@@ -3,21 +3,18 @@ import re
 import time
 
 input = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data.txt")
-with open(input, "r") as file:
-    a, b, c, program = re.findall(
-        r"(\d+).*?(\d+).*?(\d+).*?(\d(?:,\d)*)", file.read(), re.S
-    )[0]
-
-register_a = int(a)
-register_b = int(b)
-register_c = int(c)
-program = list(map(int, program.split(",")))
-pointer = 0
-output = []
 
 
-def get_combo_operand(operand):
-    global register_a, register_b, register_c
+def parse_input(input: str) -> tuple:
+    with open(input, "r") as file:
+        a, b, c, program = re.findall(
+            r"(\d+).*?(\d+).*?(\d+).*?(\d(?:,\d)*)", file.read(), re.S
+        )[0]
+
+    return int(a), int(b), int(c), list(map(int, program.split(",")))
+
+
+def get_combo_operand(operand, register_a, register_b, register_c):
 
     match operand:
         case 0 | 1 | 2 | 3:
@@ -32,45 +29,68 @@ def get_combo_operand(operand):
             raise ValueError("7 is not a valid operand")
 
 
-def run_instruction(opcode: int, operand: int):
-    global register_a, register_b, register_c, pointer
-
-    match opcode:
-        case 0:
-            register_a = register_a >> get_combo_operand(operand)
-        case 1:
-            register_b = register_b ^ operand
-        case 2:
-            register_b = get_combo_operand(operand) % 8  # or &7 for bitwise AND
-        case 3:
-            pointer = operand - 2 if register_a != 0 else float("inf")
-        case 4:
-            register_b = register_b ^ register_c
-        case 5:
-            output.append(str(get_combo_operand(operand) & 7))  # or %8
-        case 6:
-            register_b = register_a >> get_combo_operand(operand)
-        case 7:
-            register_c = register_a >> get_combo_operand(operand)
-
-    pointer += 2
-
-
-def solve() -> int:
-    global pointer, program, output
+def run_program(
+    register_a: int, register_b: int, register_c: int, program: list
+) -> list:
+    pointer = 0
+    output = []
 
     while pointer < len(program):
         opcode = program[pointer]
         operand = program[pointer + 1]
 
-        print(f"Pointer: {pointer}, Opcode: {opcode}, Operand: {operand}")
-        run_instruction(opcode, operand)
+        # print(f"Pointer: {pointer}, Opcode: {opcode}, Operand: {operand}")
+        combo = get_combo_operand(operand, register_a, register_b, register_c)
+        match opcode:
+            case 0:
+                register_a = register_a >> combo
+            case 1:
+                register_b = register_b ^ operand
+            case 2:
+                register_b = combo % 8  # or &7 for bitwise AND
+            case 3:
+                pointer = operand - 2 if register_a != 0 else float("inf")
+            case 4:
+                register_b = register_b ^ register_c
+            case 5:
+                output.append(combo & 7)  # or %8
+            case 6:
+                register_b = register_a >> combo
+            case 7:
+                register_c = register_a >> combo
+        pointer += 2
 
-    return ",".join(output)
+    return output
+
+
+def search_a() -> int:
+    _, register_b, register_c, program = parse_input(input)
+    search_a = 0
+
+    for i in range(1, len(program) + 1):
+        sub = program[len(program) - i :]
+
+        search_a = search_a << 3
+
+        while True:
+            output = run_program(search_a, register_b, register_c, program)
+
+            if output == sub:
+                break
+            else:
+                search_a += 1
+
+    return search_a
 
 
 if __name__ == "__main__":
     print("-" * 10 + "PART 1" + "-" * 10)
     start_time = time.time()
-    print(f"Result: {solve()}")
+    a, b, c, program = parse_input(input)
+    print(f"Result: {run_program(a, b, c, program)}")
+    print(f"Duration: {round(time.time() - start_time, 2)} s", end="\n\n")
+
+    print("-" * 10 + "PART 2" + "-" * 10)
+    start_time = time.time()
+    print(f"Result: {search_a()}")
     print(f"Duration: {round(time.time() - start_time, 2)} s", end="\n\n")
