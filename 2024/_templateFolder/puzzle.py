a71ea708b1e3e4f750bfaf57766de0f058e9420f
Nicola Tobias Seitz <seitznicolatobias@gmail.com>
2024-12-24 10:57:10 +0100

    update template

--- 2024/_templateFolder/puzzle.py
+++ 2024/_templateFolder/puzzle.py
@@ -1,25 +1,25 @@
 import os
 import time
 
-input_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data.txt")
+INPUT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data.txt")
 
 
 def parse_file() -> list:
-    with open(input_file_path, "r") as file:
+    with open(INPUT, "r") as file:
         data = file.read().splitlines()
 
     return data
 
 
-def solve(data: list) -> int:
+def solve() -> int:
+    data = parse_file()
 
     return 0
 
 
 if __name__ == "__main__":
-    data = parse_file()
 
     print("-" * 10 + "PART 1" + "-" * 10)
     start_time = time.time()
-    print(f"Result: {solve(data)}")
+    print(f"Result: {solve()}")
     print(f"Duration: {round(time.time() - start_time, 2)} s", end="\n\n")
