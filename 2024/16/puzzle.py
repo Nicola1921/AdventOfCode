f97ac0cd266a55e591aff0ddf6add0ed680a79aa
Nicola Tobias Seitz <seitznicolatobias@gmail.com>
2024-12-25 13:46:29 +0100

    2024/16 part 2

--- 2024/16/puzzle.py
+++ 2024/16/puzzle.py
@@ -1,6 +1,7 @@
 import os
 import time
 from heapq import heappop, heappush
+from itertools import chain
 
 INPUT = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data.txt")
 
@@ -41,18 +42,27 @@ def get_char(data_map: list, pos: tuple) -> chr:
 def solve() -> int:
     data_map = parse_file()
 
-    seen = set()
-    heap = [(0, 0, find_position(data_map, START))]
+    seen = dict()
+    start_pos = find_position(data_map, START)
+    heap = [(0, 0, start_pos, [start_pos])]
 
-    while True:
-        points, dir_idx, pos = heappop(heap)
-        if data_map[pos[0]][pos[1]] == END:
-            return points
+    best_value = float("inf")
+    all_paths = []
 
-        if (dir_idx, pos) in seen:
+    while heap:
+        points, dir_idx, pos, path = heappop(heap)
+        if points > best_value:
             continue
 
-        seen.add((dir_idx, pos))
+        if data_map[pos[0]][pos[1]] == END:
+            all_paths.append(path)
+            if points < best_value:
+                best_value = points
+
+        if (dir_idx, pos) in seen and seen.get((dir_idx, pos)) < points:
+            continue
+
+        seen[(dir_idx, pos)] = points
 
         next_pos = (
             pos[0] + DIRECTIONS[dir_idx][0],
@@ -63,18 +73,20 @@ def solve() -> int:
             data_map[next_pos[0]][next_pos[1]] in {EMPTY, END}
             and (dir_idx, next_pos) not in seen
         ):
-            heappush(heap, (points + 1, dir_idx, next_pos))
+            heappush(heap, (points + 1, dir_idx, next_pos, path + [next_pos]))
 
         if (n_dir_idx := (dir_idx - 1) % 4, pos) not in seen:
-            heappush(heap, (points + 1000, n_dir_idx, pos))
+            heappush(heap, (points + 1000, n_dir_idx, pos, path))
 
         if (n_dir_idx := (dir_idx + 1) % 4, pos) not in seen:
-            heappush(heap, (points + 1000, n_dir_idx, pos))
+            heappush(heap, (points + 1000, n_dir_idx, pos, path))
+
+    return best_value, len(set(chain.from_iterable(all_paths)))
 
 
 if __name__ == "__main__":
 
-    print("-" * 10 + "PART 1" + "-" * 10)
+    print("-" * 10 + "PART 1 & 2" + "-" * 10)
     start_time = time.time()
     print(f"Result: {solve()}")
     print(f"Duration: {round(time.time() - start_time, 2)} s", end="\n\n")
